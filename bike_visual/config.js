d381d1acbbad5eab6155a86aa735f7ee709aa590
Connie Siu <csiu22@mit.edu>
2016-01-13 16:21:28 -0500

    Still trying connection

--- bike_visual/config.js
+++ bike_visual/config.js
@@ -1,5 +1,4 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://18.85.28.250/:5432/bicycle';
-
+var connectionString = process.env.DATABASE_URL || 'postgres://csiu22:csiu22@18.85.28.250/bicycle';
 module.exports = connectionString;
 
 
