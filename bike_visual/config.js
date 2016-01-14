fd6ad71ccb512920a8757f3633095e7f9d1deb4f
csiu22 <csiu22@mit.edu>
2016-01-14 14:17:58 -0500

    FINALLY FIXED POSTGRES SOCKET STRING

--- bike_visual/config.js
+++ bike_visual/config.js
@@ -1,4 +1,4 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://csiu22@localhost/bicycle';
+var connectionString = process.env.DATABASE_URL || '/var/run/postgresql bicycle';
 //var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/bicycle';
 
 module.exports = connectionString;
